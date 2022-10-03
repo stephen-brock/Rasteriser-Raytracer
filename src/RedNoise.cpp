2573362947856c2feb754830843b75f4d42c40dd
Stephen Brock <uq20042@bristol.ac.uk>
2022-10-03 14:27:01 +0100

    Workbook 2 complete

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -1,5 +1,6 @@
 #include <CanvasTriangle.h>
 #include <DrawingWindow.h>
+#include <glm/glm.hpp>
 #include <Utils.h>
 #include <fstream>
 #include <vector>
@@ -7,13 +8,39 @@
 #define WIDTH 320
 #define HEIGHT 240
 
-void draw(DrawingWindow &window) {
+std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues)
+{
+	std::vector<glm::vec3> v;
+	for (int i = 0; i < numberOfValues; i++)
+	{
+		glm::vec3 value = from + (to - from) * ((float)i / (numberOfValues - 1));
+		v.push_back(value);
+	}
+
+	return v;
+}
+
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) 
+{
+	std::vector<float> v;
+	for (int i = 0; i < numberOfValues; i++)
+	{
+		float value = from + (to - from) * ((float)i / (numberOfValues - 1));
+		v.push_back(value);
+	}
+
+	return v;
+	
+}
+
+void draw(DrawingWindow &window, std::vector<std::vector<glm::vec3>> &pixels) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
 		for (size_t x = 0; x < window.width; x++) {
-			float red = rand() % 256;
-			float green = 0.0;
-			float blue = 0.0;
+			glm::vec3 col = pixels[y][x];
+			float red = col.x;
+			float green = col.y;
+			float blue = col.z;
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
 			window.setPixelColour(x, y, colour);
 		}
@@ -35,10 +62,19 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+	std::vector<std::vector<glm::vec3>> pixels;
+	std::vector<float> green = interpolateSingleFloats(0, 255, window.height);
+	for (int j = 0; j < window.height; j++)
+	{
+		glm::vec3 from = glm::vec3(255, green[j], 0);
+		glm::vec3 to = glm::vec3(0, green[j], 255 - green[j]);
+		pixels.push_back(interpolateThreeElementValues(from, to, window.width));
+	}
+	
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+		draw(window, pixels);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
