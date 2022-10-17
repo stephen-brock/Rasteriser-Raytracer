e84841a69cd0af637c690c30b7f7300ce7051039
Stephen Brock <uq20042@bristol.ac.uk>
2022-10-17 17:07:15 +0100

    Load OBJ file

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -8,6 +8,11 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ModelTriangle.h>
+#include <unordered_map>
 
 #define WIDTH 320
 #define HEIGHT 240
@@ -141,6 +146,91 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numberOfVal
 	return v;
 }
 
+int getSubStringIndex(std::string s, char delimiter, int startIndex)
+{
+	for (int i = startIndex; i < s.length(); i++)
+	{
+		if (s[i] == delimiter) 
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+std::string stringRange(std::string s, int from, int to)
+{
+	std::string sub = "";
+	for (int i = from; i < to; i++)
+	{
+		sub += s[i];
+	}
+	
+	return sub;
+}
+
+glm::vec3 vec3FromString(std::string s, float scale)
+{
+	int xTo = getSubStringIndex(s, ' ', 2);
+	float x = std::stof(stringRange(s, 2, xTo));
+	int yTo = getSubStringIndex(s, ' ', xTo + 1);
+	float y = std::stof(stringRange(s, xTo + 1, yTo));
+	float z = std::stof(stringRange(s, yTo + 1, s.length()));
+
+	return glm::vec3(x * scale, y * scale, z * scale);
+}
+
+ModelTriangle triFromString(std::string s, std::vector<glm::vec3> &verts)
+{
+	int xTo = getSubStringIndex(s, ' ', 2);
+	int x = std::stoi(stringRange(s, 2, xTo)) - 1;
+	int yTo = getSubStringIndex(s, ' ', xTo + 1);
+	int y = std::stoi(stringRange(s, xTo + 1, yTo)) - 1;
+	int z = std::stoi(stringRange(s, yTo + 1, s.length())) - 1;
+	return ModelTriangle(verts[x], verts[y], verts[z], Colour(255,255,255));
+}
+
+std::unordered_map<std::string, Colour> loadMtl(std::string path)
+{
+	std::unordered_map<std::string, Colour> materials();
+	
+	std::string line;
+	std::ifstream file(path);
+
+
+	return materials;
+}
+
+std::vector<ModelTriangle> loadObj(std::string path, float scale) 
+{
+	std::string line;
+	std::ifstream file(path);
+	std::vector<ModelTriangle> triangles;
+	std::vector<glm::vec3> verts;
+	while (getline(file, line))
+	{
+		if (line[0] == 'o') 
+		{
+			while (getline(file, line) && line.length() > 0) 
+			{
+				if (line[0] == 'v')
+				{
+					verts.push_back(vec3FromString(line, scale));
+				}
+				else if (line[0] == 'f')
+				{
+					triangles.push_back(triFromString(line, verts));
+				}
+			}
+		}
+	}
+
+	file.close();
+
+	return triangles;
+}
+
 void handleEvent(SDL_Event event, DrawingWindow &window) {
 	if (event.type == SDL_KEYDOWN) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
@@ -184,6 +274,11 @@ int main(int argc, char *argv[]) {
 
 	fillTriangle(tri, map, window);
 	strokeTriangle(tri, Colour(255,255,255), window);
+	
+	std::vector<ModelTriangle> model = loadObj("/Users/smb/Desktop/RedNoise/src/cornell-box.obj", 0.35f);
+	
+	
+	std::cout << model[0] << std::endl;
 	
 
 	while (true) {
